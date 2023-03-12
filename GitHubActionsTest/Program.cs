49a353bae41201f3129c50caade1f94ae3c02241
MamadyKonate <L00177597@atu.ie>
2023-03-12 16:41:20 +0000

    Just added commenttCreate superlinter.yml
    Superlinter.yml added to the main for the initial GitHub Actions run

--- GitHubActionsTest/Program.cs
+++ GitHubActionsTest/Program.cs
@@ -8,6 +8,10 @@ namespace GitHubActionsTest
 {
     internal class Program
     {
+        /// <summary>
+        /// Main method
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
